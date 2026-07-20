56058c509a24b0073634cf4da1e4a4b7f7506fcd
Saeed. Ghavami <wordcaravan.books@gmail.com>
2026-07-20 19:53:02 +0330

    Fix bot initialization in bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,6 @@
 const TelegramBot = require("node-telegram-bot-api");
 
-
-const bot = new TelegramBot("const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true });", { polling: true });
+const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true });
 
 // دستور /start
 bot.onText(/\/start/, (msg) => {
