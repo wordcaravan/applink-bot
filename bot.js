3b72a3bd9533a1c0fb7c1c20b00c4dff87c11238
Saeed. Ghavami <wordcaravan.books@gmail.com>
2026-07-20 20:52:07 +0330

    Add HTTP server setup to bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,7 @@
-const TelegramBot = require("node-telegram-bot-api");
+const http = require("http");
+http.createServer(() => {}).listen(process.env.PORT || 3000);
 
+const TelegramBot = require("node-telegram-bot-api");
 const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true });
 
 // دستور /start
